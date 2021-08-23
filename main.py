9592cf0461420e72263e0d3477c6aef354b9cea2
Martina <a.martina.eriksson@hotmail.com>
2021-08-23 16:28:28 +0200

    get from api

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask import request, jsonify
+
 from flask_mysqldb import MySQL
+from instance.config import API_KEY
+from pip._vendor import requests
 
 
 app = Flask(__name__, instance_relative_config=True)
@@ -7,15 +11,33 @@ app.config.from_pyfile('config.py')
 
 mysql = MySQL(app)
 
-@app.route('/')
+# @app.route('/')
+# def recipes():
+#     cur = mysql.connection.cursor()
+#     cur.execute('''SELECT * FROM recipe''')
+#     rows = cur.fetchall()
+#     recipes = []
+#     for row in rows:
+#         recipes.append(row['name'])
+#     return str(recipes)
+
+
+# https://api.spoonacular.com/recipes/716429/information?apiKey=YOUR-API-KEY&includeNutrition=true. 
+# https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2
+# get('https://api.spoonacular.com/recipes/findByIngredients?information&apiKey=' + API_KEY + '&ingredients=apples,+flour,+sugar&number=2')
+
+@app.route('/recipes')
 def recipes():
-    cur = mysql.connection.cursor()
-    cur.execute('''SELECT * FROM recipe''')
-    rows = cur.fetchall()
-    recipes = []
-    for row in rows:
-        recipes.append(row['name'])
-    return str(recipes)
+    
+    res = requests.get('https://api.spoonacular.com/recipes/findByIngredients?information&apiKey=' + API_KEY + '&ingredients=apples,+flour,+sugar&number=2')
+    json_data = res.json()
+    recipe_list = []
+
+    for data in json_data:
+        recipe_list.append(data['title'])
+    return str(recipe_list)
+    
+
 
 if __name__ == '__main__':
     app.run(debug=True)
