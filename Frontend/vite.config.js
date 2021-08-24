e0c7ef3af442ce65ad1f097e2886174bf4bf9ad3
Martina <a.martina.eriksson@hotmail.com>
2021-08-24 15:53:17 +0200

    added proxy

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,5 +3,18 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: 
+    {
+      fs: {
+        allow: ['..'] // Allow access to files in project root
+      },
+      proxy: {
+        '/api': {
+          target: 'http://localhost:5000',
+          changeOrigin: true
+        }
+      }
+    }
+  
 })
