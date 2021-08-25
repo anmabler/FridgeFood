cd1dbc00322bc97d5dc997509e39ef9c94c4a53d
Martina <a.martina.eriksson@hotmail.com>
2021-08-25 15:30:09 +0200

    added findById

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import request, jsonify
 from flask_mysqldb import MySQL
 from instance.config import API_KEY
 from pip._vendor import requests
+import json
 
 
 app = Flask(__name__, instance_relative_config=True)
@@ -14,7 +15,7 @@ mysql = MySQL(app)
 @app.route('/')
 def greeting():
     return "Hello from python"
-# def recipes():
+# def db():
 #     cur = mysql.connection.cursor()
 #     cur.execute('''SELECT * FROM recipe''')
 #     rows = cur.fetchall()
@@ -23,26 +24,22 @@ def greeting():
 #         recipes.append(row['name'])
 #     return str(recipes)
 
-@app.route('/api')
-def api():
-    return "this is the api route"
-
-
 # https://api.spoonacular.com/recipes/716429/information?apiKey=YOUR-API-KEY&includeNutrition=true. 
 # https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2
 # get('https://api.spoonacular.com/recipes/findByIngredients?information&apiKey=' + API_KEY + '&ingredients=apples,+flour,+sugar&number=2')
-
-@app.route('/recipes')
-def recipes():
-    
+@app.route('/api/recipes/findByIngredients')
+def findByIngredients():
     res = requests.get('https://api.spoonacular.com/recipes/findByIngredients?information&apiKey=' + API_KEY + '&ingredients=apples,+flour,+sugar&number=2')
-    json_data = res.json()
-    recipe_list = []
+    data = json.loads(res.content)
+    return jsonify(data)
 
-    for data in json_data:
-        recipe_list.append(data['title'])
-    return str(recipe_list)
-    
+@app.route('/api/recipes/findById/<int:id>')
+def findById(id):
+    # https://api.spoonacular.com/recipes/{id}/information
+    # https://api.spoonacular.com/recipes/716429/information?includeNutrition=false
+    res = requests.get('https://api.spoonacular.com/recipes/' + str(id) + '/information?apiKey=' + API_KEY + '&includeNutrition=false')
+    data = json.loads(res.content)
+    return jsonify(data)
 
 
 if __name__ == '__main__':
