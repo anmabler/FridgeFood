91b951b2271879340ccebe8af0f14ca73393c5ca
Martina <a.martina.eriksson@hotmail.com>
2021-09-07 21:39:32 +0200

    post working

--- Frontend/src/views/UserPage.vue
+++ Frontend/src/views/UserPage.vue
@@ -4,18 +4,42 @@
       <input type="text" id="ingredient" v-model="ingredient">
       <button type="submit">Add</button>
   </form>
+  <div v-if="this.ingredient.length">{{ ingredient }}</div>
 </template>
 
 <script>
 export default {
     data (){
         return {
-            ingredient:null
+            ingredient:'',
+
         }
     },
     methods: {
+         async postIngredients(){
+            try{
+                let request = {
+                    method: "POST",
+                    url: "http://localhost:3000/users/1" ,
+                    headers: { "Content-Type": "application/json"},
+                    body: JSON.stringify({ 
+                        name: this.ingredient })
+     
+                }
+                let response = await fetch("http://localhost:3000/api/users/1", request)
+                let data = await response.json()
+                this.ingredient = data 
+            } catch(error){
+                console.log(error)
+            }
+
 
         }
+
+        }
+
+
+        
     }
 
 
